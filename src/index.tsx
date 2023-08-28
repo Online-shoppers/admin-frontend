cc860e48e3a1c3f914f8bf527c266125295f711e
Vladzlaf <vladzlafm@gmail.com>
2023-08-28 17:39:05 +0300

    feat: added i18n init

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
+import App from 'App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './app';
+import './i18n';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
