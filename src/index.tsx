ff813a5a1f87839db3ea7f4f4bd2a3e4eb1f09c2
Vladzlaf <vladzlafm@gmail.com>
2023-08-28 18:11:02 +0300

    fix: deleted duplicate identifier App

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './i18n';
-import App from './App';
-
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
