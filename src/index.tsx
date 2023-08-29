4b40a455b70f40b3137e7ddaedd52ff0d695d09e
Vladzlaf <vladzlafm@gmail.com>
2023-08-29 14:22:48 +0300

    fix: files app and index

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import App from 'app';
+
+import store from 'store';
+
+import './i18n';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+);
+
+reportWebVitals();
+
+serviceWorkerRegistration.register();
