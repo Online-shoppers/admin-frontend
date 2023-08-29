034370d053ef83f1848662565584d69f62a3a1d0
Vladzlaf <vladzlafm@gmail.com>
2023-08-29 11:21:19 +0300

    feat: added service-worker.ts to admin

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import { Provider } from 'react-redux';
 import store from 'store';
 
 import './i18n';
-
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -22,3 +22,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorkerRegistration.register();
