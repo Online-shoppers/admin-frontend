1c94e525390e1caa0e9cf44fae40760570c0e7d7
ivanm <ivanmatsevich1@gmail.con>
2023-08-30 22:10:17 +0300

    feat: page styled accs

--- src/app/products/accessory.page.tsx
+++ src/app/products/accessory.page.tsx
@@ -1,11 +1,8 @@
-import { CardMedia } from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
-import { queryKey } from '@tanstack/react-query/build/lib/__tests__/utils';
-import axios from 'axios';
 import React from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
@@ -42,74 +39,78 @@ export const Accessory = () => {
     const response = await updateAccessoryInfo(productId, data);
     console.log(response);
   };
-
   return (
-    <Box>
+    <Box p={3}>
       {accessoryQuery.isLoading ? (
         <Typography>Loading...</Typography>
       ) : accessoryQuery.isError ? (
         <Typography>Error loading data</Typography>
       ) : (
-        <div>
-          <Typography variant="h4">{category}</Typography>
-          {accessoryQuery && (
-            <Box>
-              <CardMedia
-                component="img"
-                height="140"
-                image={accessoryQuery.data.image_url} // Assuming product has an 'image_url' property
-              />
+        <Box display="flex" justifyContent="center">
+          <Box width="80%" p={3} boxShadow={3} display="flex">
+            <Box
+              component="img"
+              sx={{ height: 400, width: 400, marginRight: 7, marginTop: 3 }}
+              src={accessoryQuery.data.image_url}
+              alt={accessoryQuery.data.name}
+            />
+            <Box flex="1" ml={2} display="flex" flexDirection="column">
+              <Typography variant="h4" sx={{ paddingBottom: 3 }}>
+                {accessoryQuery.data.name}
+              </Typography>
               <form onSubmit={handleSubmit(onSubmit)}>
-                <Controller
-                  name="name"
-                  control={control}
-                  defaultValue={accessoryQuery.data.name}
-                  render={({ field }) => <TextField {...field} label="Name" />}
-                />
-                <Controller
-                  name="price"
-                  control={control}
-                  defaultValue={accessoryQuery.data.price}
-                  render={({ field }) => <TextField {...field} label="Price" />}
-                />
-                <Controller
-                  name="description"
-                  control={control}
-                  defaultValue={accessoryQuery.data.description}
-                  render={({ field }) => <TextField {...field} label="Description" />}
-                />
-                <Controller
-                  name="quantity"
-                  control={control}
-                  defaultValue={accessoryQuery.data.quantity}
-                  render={({ field }) => <TextField {...field} type="number" label="Quantity" />}
-                />
-                <Controller
-                  name="weight"
-                  control={control}
-                  defaultValue={accessoryQuery.data.weight}
-                  render={({ field }) => <TextField {...field} label="Weight" />}
-                />
-                <Controller
-                  name="image_url"
-                  control={control}
-                  defaultValue={accessoryQuery.data.image_url}
-                  render={({ field }) => <TextField {...field} label="Image URL" />}
-                />
-                <Controller
-                  name="archived"
-                  control={control}
-                  defaultValue={accessoryQuery.data.archived}
-                  render={({ field }) => <TextField {...field} label="Archived" />}
-                />
+                <Box display="flex" flexDirection="column" gap={2}>
+                  <Controller
+                    name="name"
+                    control={control}
+                    defaultValue={accessoryQuery.data.name}
+                    render={({ field }) => <TextField {...field} label="Name" />}
+                  />
+                  <Controller
+                    name="price"
+                    control={control}
+                    defaultValue={accessoryQuery.data.price}
+                    render={({ field }) => <TextField {...field} label="Price" type="number" />}
+                  />
+                  <Controller
+                    name="description"
+                    control={control}
+                    defaultValue={accessoryQuery.data.description}
+                    render={({ field }) => <TextField {...field} label="Description" />}
+                  />
+                  <Controller
+                    name="quantity"
+                    control={control}
+                    defaultValue={accessoryQuery.data.quantity}
+                    render={({ field }) => <TextField {...field} type="number" label="Quantity" />}
+                  />
+                  <Controller
+                    name="weight"
+                    control={control}
+                    defaultValue={accessoryQuery.data.weight}
+                    render={({ field }) => <TextField {...field} label="Weight" />}
+                  />
+                  <Controller
+                    name="image_url"
+                    control={control}
+                    defaultValue={accessoryQuery.data.image_url}
+                    render={({ field }) => <TextField {...field} label="Image URL" />}
+                  />
+                  <Controller
+                    name="archived"
+                    control={control}
+                    defaultValue={accessoryQuery.data.archived}
+                    render={({ field }) => <TextField {...field} label="Archived" />}
+                  />
 
-                <Button type="submit" variant="contained">
-                  Save Changes
-                </Button>
+                  <Button type="submit" variant="contained">
+                    Save Changes
+                  </Button>
+                </Box>
               </form>
             </Box>
-          )}
-        </div>
+          </Box>
+        </Box>
       )}
     </Box>
   );
